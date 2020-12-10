ca6d5bc5959228bf6568b6f0393465aa2a061758
Tony Lin <tonylinbusiness@gmail.com>
2020-12-09 21:21:13 -0500

    Added list and detail for issuescontroller

---
+++ Jello/Areas/Issues/IssuesController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Jello.Areas.Issues
+{
+    [Authorize]
+    [Menu("Issues")]
+    [Route("issues")]
+    public class IssuesController : Controller
+    {
+        #region CRUD
+
+        [HttpGet]
+        public async Task<IActionResult> List(List model) => await model.GetAsync();
+
+        [HttpGet("{id}", Order = 10)]
+        public async Task<IActionResult> Detail(Detail model) => await model.GetAsync();
+
+        // [HttpGet("edit/{id?}")]
+        // public async Task<IActionResult> Edit(int id) => await new Edit { Id = id }.GetAsync();
+
+        // [HttpPost("edit/{id?}")]
+        // public async Task<IActionResult> Edit(Edit model) => await model.PostAsync();
+
+        // [HttpPost("delete"), AjaxOnly]
+        // public async Task<IActionResult> Delete(Delete model) => await model.PostAsync();
+
+        #endregion
+
+        #region Secondary Methods
+
+        // [HttpGet("import")]  // Step 1
+        // public IActionResult Import() => new Import().Get();
+
+        // [HttpPost("import")]  // Step 2
+        // public async Task<IActionResult> Import(IFormFile file) => await new Import { FormFile = file }.Upload();
+
+        // [HttpPost("import/go")]   // Step 3
+        // public async Task<IActionResult> Import(Import model) => await model.PostAsync();
+
+        // [HttpGet("export")]
+        // public async Task<IActionResult> Export(Export model) => await model.GetAsync();
+
+        #endregion
+    }
+}
---
+++ Jello/Areas/Issues/List.cs
@@ -0,0 +1,112 @@
+using Jello.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jello.Areas.Issues
+{
+    public class List : PagedModel<Detail>
+    {
+        #region Data
+
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+
+        public int? ProjectId { get; set; }
+        public string ProjectTitle { get; set; }
+
+        public int? AssigneeId { get; set; }
+        public string AssigneeName { get; set; }
+
+        public int? OwnerId { get; set; }
+
+        #endregion
+
+        #region Handlers
+
+        public override async Task<IActionResult> GetAsync()
+        {
+            var query = BuildQuery();
+
+            TotalRows = await query.CountAsync();
+            var items = await query.Skip(Skip).Take(Take).ToListAsync();
+
+            _mapper.Map(items, Items);
+
+            return View(this);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        protected IQueryable<Issue> BuildQuery()
+        {
+            var query = _db.Issue.AsQueryable();
+
+            // Filters
+
+            if (AdvancedFilter)
+            {
+                if (Title != null)
+                {
+                    query = query.Where(c => c.Title.Contains(Title));
+                }
+
+                if (ProjectTitle != null)
+                {
+                    query = query.Where(c => c.ProjectTitle.Contains(ProjectTitle));
+                }
+
+                if (AssigneeId != null)
+                {
+                    query = query.Where(c => c.AssigneeId == AssigneeId.Value);
+                }
+
+                if (OwnerId != null)
+                {
+                    query = query.Where(c => c.OwnerId == OwnerId.Value);
+                }
+            }
+            else
+            {
+                switch (Filter)
+                {
+                    case 1: query = query.Where(c => _viewedService.GetIds("Issue").Contains(c.Id)); break;
+                    case 2: query = query.Where(c => c.OwnerId == _currentUser.Id); break;
+                }
+            }
+
+            // Sorting
+
+            query = Sort switch
+            {
+                "Id" => query.OrderBy(c => c.Id),
+                "-Id" => query.OrderByDescending(c => c.Id),
+                "Title" => query.OrderBy(c => c.Title),
+                "-Title" => query.OrderByDescending(c => c.Title),
+                "Type" => query.OrderBy(c => c.Type),
+                "-Type" => query.OrderByDescending(c => c.Type),
+                "OwnerAlias" => query.OrderBy(c => c.OwnerAlias),
+                "-OwnerAlias" => query.OrderByDescending(c => c.OwnerAlias),
+                "CreatedDate" => query.OrderBy(c => c.CreatedDate),
+                "-CreatedDate" => query.OrderByDescending(c => c.CreatedDate),
+                _ => query.OrderByDescending(c => c.Id),
+            };
+
+            return query;
+        }
+
+        #endregion
+
+        #region Mapping
+
+        // Mappings are already defined in the associated Detail.cs page
+
+        #endregion
+    }
+}
