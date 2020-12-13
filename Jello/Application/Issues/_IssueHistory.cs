b61bda839c7252e6b447bafc6d9cb741f05d21b9
Tony Lin <tonylinbusiness@gmail.com>
2020-12-13 14:35:50 -0500

    Added issue history page

---
+++ Jello/Application/Issues/_IssueHistory.cs
@@ -0,0 +1,12 @@
+namespace Dofactory.CRM.Core.Application.Issues
+{
+    public class _IssueHistory
+    {
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public string CreatedDate{ get; set; }
+        public string OwnerName { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
