0bc0c27ee0b234ac26e848ac2fb5a477fb7274c6
Tony Lin <tonylinbusiness@gmail.com>
2020-12-14 11:01:48 -0500

    Added user class

---
+++ Jello/Application/Auth/_User.cs
@@ -0,0 +1,13 @@
+namespace Jello.Application.Auth
+{
+    public class _User
+    {
+        public int? UserId { get; set; }
+        public string User { get; set; }
+        public string Role { get; set; }
+        public string Color { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
