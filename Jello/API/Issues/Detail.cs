0071060c825527aec3874e0cd7c25061d3bfe104
Tony Lin <tonylinbusiness@gmail.com>
2020-12-27 13:59:51 -0500

    added issues to api

---
+++ Jello/API/Issues/Detail.cs
@@ -0,0 +1,79 @@
+using Jello.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jello.API.Issues
+{
+    public class Detail : BaseModel
+    {
+        #region Data
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+
+        public int? AssigneeId { get; set; }
+        public string AssigneeName { get; set; }
+
+        public int ProjectId { get; set; }
+        public string ProjectTitle { get; set; }
+
+        public int? OwnerId { get; set; }
+        public string OwnerAlias { get; set; }
+        public string OwnerName { get; set; }
+        
+        public DateTime CreatedDate { get; set; }
+
+        // Related Lists (none)
+        public string Tab { get; set; } = "details";
+
+        // History
+        // public List<_IssueHistory> IssueHistory { get; set; } = new List<_IssueHistory>();
+
+        #endregion
+
+        #region Handlers
+
+        public override async Task<IActionResult> GetAsync()
+        {
+            var issue = await _db.Issue.SingleOrDefaultAsync(c => c.Id == Id);
+            _mapper.Map(issue, this);
+            
+            // var history = await _db.IssueHistory.Where(o => o.IssueId == issue.Id).ToListAsync();
+            // _mapper.Map(history, IssueHistory);
+
+            await _viewedService.Log(Id, "Issue", issue.Title);
+
+            return View(this);
+        }
+
+        #endregion
+
+        #region Mapping
+
+        public class MapperProfile : BaseProfile
+        {
+            public MapperProfile()
+            {
+                CreateMap<Issue, Detail>()
+                  .Map(dest => dest.OwnerName, opt => opt.MapFrom(src => _cache.Users[src.OwnerId].Name));
+
+                // CreateMap<IssueHistory, _IssueHistory>()
+                //     .Map(dest => dest.Type, opt => opt.MapFrom(src => src.Type))
+                //     .Map(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                //     .Map(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority))
+                //     .Map(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate.ToDate()))
+                //     .Map(dest => dest.OwnerName, opt => opt.MapFrom(src => _cache.Users[src.OwnerId].Name));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Jello/API/Issues/IssuesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static Jello.API.Issues.List;
+
+namespace Jello.API.Issues
+{
+    [ApiController]
+    // [Authorize]
+    [Menu("Issues")]
+    [Route("api/[controller]")]
+    public class IssuesController : Controller
+    {
+        [HttpGet]
+        public async Task<Output> List([FromQuery]List model) => await model.GetAsync();
+
+        // [HttpGet("{id}", Order = 10)]
+        // public async Task<DetailOutput> Detail([FromQuery]Detail model) => await model.GetAsync();
+
+        // [HttpGet("edit/{id?}")]
+        // public async Task<IActionResult> Edit(int id) => await new Edit { Id = id }.GetAsync();
+
+        // [HttpPost("edit/{id?}")]
+        // public async Task<IActionResult> Edit(Edit model) => await model.PostAsync();
+
+        // [HttpPost("delete/{id?}")]
+        // public async Task<IActionResult> Delete(Delete model) => await model.PostAsync();
+
+        // [HttpPost("delete/{id?}")]
+        // public async Task<IActionResult> Delete(int id) => await new Delete { Id = id }.PostAsync();
+    }
+}
